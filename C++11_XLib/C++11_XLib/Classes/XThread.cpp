0bb515feefb07261534fc2c8884d2c45d74b90fc
ximena <ximena@Ximena.local>
2016-05-26 20:08:15 +0800

    add thread pool

---
+++ C++11_XLib/C++11_XLib/Classes/XThread.cpp
@@ -0,0 +1,71 @@
+//----------------------------------------------//
+//  XThread.cpp
+//  C++11_XLib
+//
+//  Copyright (c) __DATA__  Ximena.
+//  Created by ximena on 16/5/26.
+//    Copyright © 2016年 ximena. All rights reserved.
+//  Contact Email: xiaominghe2014@gmail.com
+//----------------------------------------------//
+#include "XThread.h"
+#include <assert.h>
+#include <utility>
+
+#include "XDefine.h"
+using namespace std;
+
+template class XTaskQueue<Task_void>;
+
+XThreadPool::XThreadPool(int threads,int maxWaiting,bool start):
+mTaskQueue(maxWaiting),mThreads(threads)
+{
+    if(start)
+    {
+        this->start();
+    }
+}
+
+XThreadPool::~XThreadPool()
+{
+    assert(mTaskQueue.isExited());
+    if(mTaskQueue.capacity())
+    {
+        LOG_E("%lu tasks not processed when thread pool exited\n",mTaskQueue.capacity());
+    }
+}
+
+void XThreadPool::start()
+{
+    for(auto &th: mThreads)
+    {
+        thread t([this]
+        {
+            while (!mTaskQueue.isExited())
+            {
+                Task_void task;
+                if(mTaskQueue.popWait(&task))
+                {
+                    task();
+                }
+            }
+        });
+        th.swap(t);
+    }
+}
+
+void XThreadPool::join()
+{
+    for(auto &e:mThreads)
+        e.join();
+}
+
+void XThreadPool::detach()
+{
+    for(auto &e:mThreads)
+        e.detach();
+}
+
+bool XThreadPool::addTask(Task_void && task)
+{
+    return mTaskQueue.push(move(task));
+}
--- C++11_XLib/C++11_XLib/main.cpp
+++ C++11_XLib/C++11_XLib/main.cpp
@@ -7,16 +7,36 @@
 //
 #include <iomanip>
 #include "Classes/XDefine.h"
+#include "Classes/XThread.h"
 int main(int argc, const char * argv[]) {
     
-    XTime::getInstance()->doPertime(10, 1,[]
+    auto fun = []
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(rand()%50));
+        std::thread::id tid = std::this_thread::get_id();
+        auto t = XTime::getTimeFromTimestamp_milliseconds(XTime::getTimestamp_milliseconds(),8);
+        LOG_I("thread_id=%d,%02d:%02d:%02d",tid,t->tm_hour,t->tm_min,t->tm_sec);
+    };
+    auto pool = new XThreadPool(5,5,true);
+    
+    pool->addTask(fun);
+    pool->addTask(fun);
+    pool->addTask(fun);
+    pool->addTask(fun);
+    
+    XTime::getInstance()->doPertime(-1, 1,[]
     {
         auto t = XTime::getTimeFromTimestamp_milliseconds(XTime::getTimestamp_milliseconds(),8);
         LOG_D("%02d:%02d:%02d",t->tm_hour,t->tm_min,t->tm_sec);
     } );
-    while (true)
+    auto loop = []
     {
-        XTimer::getInstance()->mainLoop();
-    }
+        while (true)
+        {
+            XTimer::getInstance()->mainLoop();
+        }
+    };
+    pool->addTask(loop);
+    pool->join();
     return 0;
 }
