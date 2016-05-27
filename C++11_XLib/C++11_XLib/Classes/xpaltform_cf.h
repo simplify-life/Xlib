b07b67696b59dbbe3e8988a27c0e1e5cb9b3691b
ximena <ximena@Ximena.local>
2016-05-27 10:00:11 +0800

    add log filter test

---
+++ C++11_XLib/C++11_XLib/Classes/xpaltform_cf.h
@@ -0,0 +1,15 @@
+//----------------------------------------------//
+//  xpaltform_cf.h
+//  C++11_XLib
+//
+//  Copyright (c) __DATA__  Ximena.
+//  Created by ximena on 16/5/27.
+//  Copyright © 2016年 ximena. All rights reserved.
+//  Contact Email: xiaominghe2014@gmail.com
+//----------------------------------------------//
+
+#ifndef xpaltform_cf_h
+#define xpaltform_cf_h
+
+
+#endif /* xpaltform_cf_h */
