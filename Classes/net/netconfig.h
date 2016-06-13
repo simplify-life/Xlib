0a606d19af2f2cc653b124942b7a6b6807209c0b
ximena <ximena@Ximena.local>
2016-06-13 17:38:36 +0800

    add macros for reduce

---
+++ Classes/net/netconfig.h
@@ -0,0 +1,15 @@
+//----------------------------------------------//
+//  netconfig.h
+//  xlib
+//
+//  Copyright (c) __DATA__  Ximena.
+//  Created by ximena on 16/6/13.
+//  Copyright © 2016年 ximena. All rights reserved.
+//  Contact Email: xiaominghe2014@gmail.com
+//----------------------------------------------//
+
+#ifndef netconfig_h
+#define netconfig_h
+
+
+#endif /* netconfig_h */
